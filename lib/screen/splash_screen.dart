03abd91d4a5fbb18876d5d2139669397506e133d
WildanFadhil <tosihiroyamada2@.com>
2024-01-21 23:09:40 +0700

    slashing screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -12,14 +12,39 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff1C1A29),
-      body: Expanded(
-        child: Stack(children: [
-          Positioned(
-              bottom: 0,
-              right: 0,
-              left: 0,
-              child: Image.asset("assets/splash_screen_illustration.png"))
-        ]),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: SafeArea(
+          child: Stack(children: [
+            Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: SizedBox(
+                    width: double.infinity,
+                    child:
+                        Image.asset("assets/splash_screen_illustration.png"))),
+            Padding(
+              padding: EdgeInsets.only(top: 148),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    textAlign: TextAlign.center,
+                    "NONTON",
+                    style: TextStyle(fontSize: 32, color: Colors.white),
+                  ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    ".ID",
+                    style: TextStyle(fontSize: 32, color: Color(0xffFFDE33)),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
